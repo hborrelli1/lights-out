dadcd24453f1299fd4f0f1526f0add268621acde
Harry Borrelli <hborrelli@outlook.com>
2020-08-05 10:20:53 -0600

    Add style to header. Setup app structure.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+import { GameBoard } from '../GameBoard/GameBoard';
+
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <h1>Lights Out</h1>
+        <p>Clicking on a cell toggles that cell and each of its immediate neighbors. The goal is to turn out all the lights, ideally with the minimum number of clicks.</p>
+      </header>
+      <main>
+        <section className="game-board">
+          <GameBoard />
+        </section>
+      </main>
     </div>
   );
 }
