564e80dd94b7a5e76fd6fbcd1e1d10fe6fdabf3e
Harry Borrelli <hborrelli@outlook.com>
2020-08-05 10:41:31 -0600

    Refactor App JSX

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -32,9 +32,7 @@ function App() {
         <p>Clicking on a cell toggles that cell and each of its immediate neighbors. The goal is to turn out all the lights, ideally with the minimum number of clicks.</p>
       </header>
       <main>
-        <section className="game-board">
-          <GameBoard squares={squares} />
-        </section>
+        <GameBoard squares={squares} />
       </main>
     </div>
   );
